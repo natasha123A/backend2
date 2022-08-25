f92de5a8d20685617b9498039e0cef9099cd0f9d
nishtha <1834810005.cse.psitcoe@gmail.com>
2022-08-25 13:51:11 +0530

    initialcommit

---
+++ config/env.js
@@ -0,0 +1,10 @@
+
+const env = {
+  database: "studentmarksheetmanagementdb",
+  username: "myserver@febeserver",
+  password: "nishtha123A",
+  host: "febeserver.mysql.database.azure.com",
+  dialect: "mysql",
+};
+
+module.exports = env;
