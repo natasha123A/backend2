1248005465548393496d3b78d34fc8abb5c93d3c
natasha123A <111418692+natasha123A@users.noreply.github.com>
2022-09-19 17:40:21 +0530

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,9 +1,9 @@
 
 const env = {
-  database: "studentmarksheetmanagementdb",
-  username: "myserver@febeserver",
-  password: "nishtha123A",
-  host: "febeserver.mysql.database.azure.com",
+  database: "products",
+  username: "myserver@myserver12367",
+  password: "jinvjung@123A",
+  host: "myserver12367.mysql.database.azure.com",
   dialect: "mysql",
 };
 
